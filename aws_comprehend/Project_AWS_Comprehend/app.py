48e063a52fee110bc4023090717c1cd116e46cdc
Vishal <86146413+BrainsCorp@users.noreply.github.com>
2024-07-01 02:44:18 -0400

    Update app.py

--- aws_comprehend/Project_AWS_Comprehend/app.py
+++ aws_comprehend/Project_AWS_Comprehend/app.py
@@ -32,6 +32,6 @@ def analyze_sentiment():
 #app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
     app.debug(True)
 
