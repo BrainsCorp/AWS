89b17fd3d5b2c31c12c1a8703a8445ef5d9946f2
Brainscorp <hellovishal12321@gmail.com>
2024-07-01 17:35:35 -0400

    updated routes

--- aws_comprehend/Project_AWS_Comprehend/app.py
+++ aws_comprehend/Project_AWS_Comprehend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION
-import os
+import json
 import boto3
 from aws_comprehend_detect import ComprehendDetect
 
@@ -11,12 +11,28 @@ comprehend_client = boto3.client(
 )
 
 
+def format_entities(entities_string):
+    formatted_text = ""
+    count = 1
+
+    # Define the regular expression pattern (adjust based on your entity data format)
+    entities_string=entities_string.replace("'", "\"")
+    entities = json.loads(entities_string)
+    formatted_text = f"Detect Entities: {len(entities)}"
+    for entity in entities:
+        formatted_text += f"\n\n{count}: {entity['Text']}\n"
+        for e in entity:
+            formatted_text += f"{e}: {entity[e]}\n"
+        count += 1
+
+    return formatted_text
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route("/", methods=["POST"])
+@app.route("/detect_entity", methods=["GET", "POST"])
 def analyze_sentiment():
     if request.method=='POST':
         input = request.form["user_input"]
@@ -28,8 +44,9 @@ def analyze_sentiment():
         response = comprehend_client.detect_entities(Text=input,
                                                    LanguageCode=language_code)
 
-    return render_template("index.html", result=f"{response['Entities']}")
-
+        #t = "[{'Score': 0.9994252920150757, 'Type': 'PERSON', 'Text': 'Barack Obama', 'BeginOffset': 0, 'EndOffset': 12}, {'Score': 0.8225329518318176, 'Type': 'QUANTITY', 'Text': '44th president', 'BeginOffset': 18, 'EndOffset': 32}]"
+        return render_template("index.html", text=input, result=format_entities(response))
+    
 #Insert the line below to to run on Cloud9
 #app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
